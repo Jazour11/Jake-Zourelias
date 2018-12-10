1fc7783d1d896cffd3992cc52cf6053e22cbe8bd
Jazour11 <JAZ66@pitt.edu>
2018-12-10 09:09:50 -0500

    I'm committing my final portfolio

---
+++ New/myjavascript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+ document.getElementById("demo").innerHTML = "jake.zourelias@gmail.com.";
+}
+window.addEventListener('DOMContentLoaded',init,false);
+
+function init() {
+    alert ('If you can see this the page has loaded');                  
+
+}
